32ea857599ea2666332305723ed43ac9ce2e65e6
Sean <sean@comsi.com>
2019-07-16 08:07:23 -0700

    minimal member events

--- bot.py
+++ bot.py
@@ -8,4 +8,12 @@ client = commands.Bot(command_prefix = "$")
 async def on_ready():
     print("Bot is ready.")
 
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server")
+
 client.run(config.token)
