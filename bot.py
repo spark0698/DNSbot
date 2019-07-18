c71986048537eb124789b8d3bc2ce31fe909f390
Sean <sean@comsi.com>
2019-07-17 19:43:09 -0700

    add users db

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import config
 import discord
 from discord.ext import commands
+import sqlite3
 
 client = commands.Bot(command_prefix = "$")
+
+conn = sqlite3.connect('users.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS users (
+             irl,
+             discord
+             )""")
 
 @client.event
 async def on_ready():
